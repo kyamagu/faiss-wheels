444c0901245405b9545432999d750331bff33a6e
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2019-07-01 18:23:37 +0900

    use pre-defined swig and openblas

--- config.sh
+++ config.sh
@@ -1,19 +1,7 @@
 function pre_build {
-    # Runs in the root directory.
-
-    # Install newer swig. RPM package is too old.
-    yum -y install pcre-devel
-    curl -sSLO https://github.com/swig/swig/archive/rel-4.0.0.tar.gz
-    tar xzf rel-4.0.0.tar.gz
-    cd swig-rel-4.0.0
-    ./autogen.sh
-    ./configure
-    make -j2
-    make install
-    cd ..
-
-    # Install OpenBLAS.
-    yum -y install openblas-devel
+    # Install build dependencies.
+    build_swig
+    build_openblas
 
     # Build binary.
     cd $REPO_DIR/..
