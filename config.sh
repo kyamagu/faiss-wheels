0fb9bf6aedb4ca587b235f3c651ad20930eea9c7
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2019-07-01 17:12:31 +0900

    fix repo_dir path

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 function pre_build {
     # Runs in the root directory.
     yum -y install swig openblas-devel
-    cd $REPO_DIR
+    cd $REPO_DIR/..
     # autoconf
     ./configure --without-cuda
     make -j2
