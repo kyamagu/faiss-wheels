43ac68a5dbe4b400cd70ae174231a79733d1090d
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2019-07-01 17:30:48 +0900

    add swig installation

--- config.sh
+++ config.sh
@@ -1,8 +1,20 @@
 function pre_build {
     # Runs in the root directory.
-    yum -y install swig openblas-devel
+
+    # Install newer swig. RPM package is too old.
+    wget https://github.com/swig/swig/archive/rel-4.0.0.tar.gz
+    tar xzf rel-4.0.0.tar.gz
+    cd rel-4.0.0
+    ./configure
+    make -j2
+    make install
+    cd ..
+
+    # Install OpenBLAS.
+    yum -y install openblas-devel
+
+    # Build binary.
     cd $REPO_DIR/..
-    # autoconf
     ./configure --without-cuda
     make -j2
     make -C python
