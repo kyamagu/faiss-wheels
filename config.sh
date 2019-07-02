9faa63b65cbf26a2b1270618c2d24ec6b2497170
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2019-07-02 13:49:38 +0900

    change pip_wheell_cmd invocation

--- config.sh
+++ config.sh
@@ -1,49 +1,21 @@
-# BUILD_DEPENDS must happen before our pre_build for Makefile build.
-function build_wheel_cmd {
-    # Builds wheel with named command, puts into $WHEEL_SDIR
-    #
-    # Parameters:
-    #     cmd  (optional, default "pip_wheel_cmd"
-    #        Name of command for building wheel
-    #     repo_dir  (optional, default $REPO_DIR)
-    #
-    # Depends on
-    #     REPO_DIR  (or via input argument)
-    #     WHEEL_SDIR  (optional, default "wheelhouse")
-    #     BUILD_DEPENDS (optional, default "")
-    #     MANYLINUX_URL (optional, default "") (via pip_opts function)
-    local cmd=${1:-pip_wheel_cmd}
-    local repo_dir=${2:-$REPO_DIR}
-    [ -z "$repo_dir" ] && echo "repo_dir not defined" && exit 1
-    local wheelhouse=$(abspath ${WHEEL_SDIR:-wheelhouse})
-    if [ -n "$BUILD_DEPENDS" ]; then
-        pip install $(pip_opts) $BUILD_DEPENDS
-    fi
-    start_spinner
-    if [ -n "$(is_function "pre_build")" ]; then pre_build; fi
-    stop_spinner
-    (cd $repo_dir && $cmd $wheelhouse)
-    repair_wheelhouse $wheelhouse
-}
-
-
 function pre_build {
-    # Install build dependencies.
-    local with_blas="-pthread -lgfortran -static-libgfortran -l:libopenblas.a"
     build_swig
     build_openblas
-    echo $PWD
+}
 
-    # Build binary.
+function pip_wheel_cmd {
+    local abs_wheelhouse=$1
+
+    local with_blas="-pthread -lgfortran -static-libgfortran -l:libopenblas.a"
     local np_include=$(python -c "import numpy as np; print(np.get_include())")
-    export CFLAGS="-I$np_include $CFLAGS"
-    # export LDFLAGS="-static -pthread"  # libgfortran dependency.
-    (cd $REPO_DIR/.. \
-        && ./configure \
-            --without-cuda \
-            --with-blas="$with_blas" \
+    (./configure \
+        --without-cuda \
+        --with-blas="$with_blas" \
+        CFLAGS="-I$np_include $CFLAGS" \
         && make -j4 \
-        && make -C python)
+        && make -C python \
+        && cd python \
+        && pip wheel $(pip_opts) -w $abs_wheelhouse --no-deps .)
 }
 
 function run_tests {
