a45af8b26a737c15e9bed40bd9dc195d201d389b
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2020-07-06 15:25:10 +0900

    Use Github Actions for CI

--- conf/cuda8.0.sh
+++
@@ -1,6 +0,0 @@
-# Pascal
-export CUDA_VERSION=8.0
-export CUDA_PKG_VERSION=8-0-8.0.61-1
-export CUBLAS_PKG_VERSION=8-0-8.0.61.2-1
-export CUDA_ARCH_FLAGS="-gencode=arch=compute_35,code=compute_35 -gencode=arch=compute_52,code=compute_52 -gencode=arch=compute_60,code=compute_60 -gencode=arch=compute_61,code=compute_61"
-export DEVTOOLSET_VERSION=4
