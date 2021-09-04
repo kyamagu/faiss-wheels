cca172c08dc7968f0fe555e29e2ba32b224e2c1c
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2021-09-05 00:18:53 +0900

    fix cross compilation for arm64

--- scripts/build_macOS.sh
+++ scripts/build_macOS.sh
@@ -2,8 +2,36 @@
 
 FAISS_OPT_LEVEL=${FAISS_OPT_LEVEL:-"generic"}
 
+HOST_ARCH=${HOST_ARCH:-$(uname -m)}
+TARGET_ARCH=${TARGET_ARCH:-$HOST_ARCH}
+if [[ ${TARGET_ARCH} == auto* || ${TARGET_ARCH} == native ]]; then
+    TARGET_ARCH=${HOST_ARCH}
+fi
+echo "TARGET_ARCH=${TARGET_ARCH}"
+
 # Install system dependencies
-brew install libomp swig
+brew install swig
+
+if [[ ${TARGET_ARCH} == ${HOST_ARCH} ]]; then
+    brew install libomp
+else
+    echo "Building libomp (${TARGET_ARCH})"
+    git clone \
+            --depth 1 \
+            --filter=blob:none \
+            --sparse \
+            https://github.com/llvm/llvm-project.git && \
+        cd llvm-project && \
+        git sparse-checkout set openmp && \
+        cd openmp && \
+        cmake . \
+            -B build \
+            -DCMAKE_BUILD_TYPE=Release \
+            -DCMAKE_OSX_ARCHITECTURES=${TARGET_ARCH} && \
+        cmake --build build -j && \
+        cmake --install build && \
+        cd ../..
+fi
 
 # Build and patch faiss
 cd faiss && \
@@ -11,10 +39,11 @@ cd faiss && \
         -B build \
         -DFAISS_ENABLE_GPU=OFF \
         -DFAISS_ENABLE_PYTHON=OFF \
-        -DBUILD_TESTING=ON \
+        -DBUILD_TESTING=OFF \
         -DFAISS_OPT_LEVEL=${FAISS_OPT_LEVEL} \
-        -DCMAKE_BUILD_TYPE=Release && \
-    cmake --build build --config Release -j && \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_OSX_ARCHITECTURES=${TARGET_ARCH} && \
+    cmake --build build --config Release -j -v && \
     cmake --install build && \
     mv faiss/python/swigfaiss.swig faiss/python/swigfaiss.i && \
     cd ..
