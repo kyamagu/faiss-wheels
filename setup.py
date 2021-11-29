6d9cd1eb03670c380623fdc05f6f2d08544b9825
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2021-11-29 15:08:53 +0900

    increment postfix version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 import os
 
 NAME = 'faiss-cpu'
-VERSION = '1.7.1.post2'
+VERSION = '1.7.1.post3'
 
 LONG_DESCRIPTION = """
 Faiss is a library for efficient similarity search and clustering of dense
