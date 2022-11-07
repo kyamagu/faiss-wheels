17a0760ac4bd0e297d7ae6d26ca6f1b3597dbf2d
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2022-11-07 23:18:32 +0900

    update cibuildwheel to support py311

--- setup.py
+++ setup.py
@@ -197,11 +197,11 @@ setup(
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
 )
