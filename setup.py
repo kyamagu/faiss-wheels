6a63749792458f5496980664b0a4eef3a5065c61
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2024-01-23 11:36:02 +0900

    Migrate metadata to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,20 +1,9 @@
 import os
 import sys
 
-from setuptools import setup
+import numpy as np
+from setuptools import Extension, setup
 from setuptools.command.build_py import build_py
-from setuptools.extension import Extension
-
-NAME = 'faiss-cpu'
-VERSION = '1.7.4'
-
-LONG_DESCRIPTION = """
-Faiss is a library for efficient similarity search and clustering of dense
-vectors. It contains algorithms that search in sets of vectors of any size, up
-to ones that possibly do not fit in RAM. It also contains supporting code for
-evaluation and parameter tuning. Faiss is written in C++ with complete wrappers
-for Python/numpy. It is developed by Facebook AI Research.
-"""
 
 # CMake variables for faiss
 FAISS_ROOT = os.getenv('FAISS_ROOT', 'faiss')
@@ -26,25 +15,12 @@ FAISS_ENABLE_GPU = (
 )
 
 
-class get_numpy_include(object):
-    """
-    Helper class to determine the numpy include path.
-
-    The purpose of this class is to postpone importing numpy
-    until it is actually installed, so that the ``get_include()``
-    method can be invoked.
-    """
-    def __str__(self):
-        import numpy as np
-        return np.get_include()
-
-
 # Platform-specific configurations
 DEFINE_MACROS = [
     ('FINTEGER', 'int'),
 ]
 INCLUDE_DIRS = [
-    get_numpy_include(),
+    np.get_include(),
     FAISS_INCLUDE,
     FAISS_ROOT,
 ]
@@ -128,7 +104,6 @@ elif sys.platform == 'darwin':
         ]
 
 if FAISS_ENABLE_GPU:
-    NAME = 'faiss-gpu'
     CUDA_HOME = os.getenv('CUDA_HOME', '/usr/local/cuda')
     INCLUDE_DIRS += [os.path.join(CUDA_HOME, 'include')]
     LIBRARY_DIRS += [os.path.join(CUDA_HOME, 'lib64')]
@@ -187,20 +162,6 @@ if FAISS_OPT_LEVEL == 'avx2':
     ext_modules.append(_swigfaiss_avx2)
 
 setup(
-    name=NAME,
-    version=VERSION,
-    description=(
-        'A library for efficient similarity search and clustering of dense '
-        'vectors.'
-    ),
-    long_description=LONG_DESCRIPTION,
-    url='https://github.com/kyamagu/faiss-wheels',
-    author='Kota Yamaguchi',
-    author_email='KotaYamaguchi1984@gmail.com',
-    license='MIT',
-    keywords='search nearest neighbors',
-    install_requires=['numpy'],
-    setup_requires=['numpy'],
     packages=['faiss', 'faiss.contrib'],
     package_dir={
         'faiss': os.path.join(FAISS_ROOT, 'faiss', 'python'),
@@ -209,19 +170,4 @@ setup(
     package_data={'faiss': ['*.i', '*.h']},
     ext_modules=ext_modules,
     cmdclass={'build_py': CustomBuildPy},
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-    ],
 )
