cbfdb17c70ee824d05f8d6d66bf3e7aa7717d6b6
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2024-01-27 22:38:18 +0900

    Update default CUDA version

--- scripts/build_Linux.sh
+++ scripts/build_Linux.sh
@@ -5,10 +5,9 @@ export CXXFLAGS="-fvisibility=hidden -fdata-sections -ffunction-sections"
 FAISS_ENABLE_GPU=${FAISS_ENABLE_GPU:-"OFF"}
 FAISS_OPT_LEVEL=${FAISS_OPT_LEVEL:-"generic"}
 
-CUDA_VERSION="11.0"
-CUDA_PKG_VERSION="11-0"
+CUDA_VERSION=${CUDA_VERSION:-"12.3"}
+CUDA_PKG_VERSION=${CUDA_PKG_VERSION:-${CUDA_VERSION//./-}}
 NVIDIA_REPO_URL="http://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo"
-CMAKE_CUDA_ARCHITECTURES="60-real;70-real;75-real;80"
 
 # Fix manylinux2014 aarch64
 if [[ $(uname -m) == "aarch64" ]]; then
@@ -37,14 +36,11 @@ if [[ ${FAISS_ENABLE_GPU} == "ON" ]]; then
         ldconfig
 
     export PATH="/opt/rh/devtoolset-7/root/usr/bin:/usr/local/cuda/bin:${PATH}"
-    export CUDAFLAGS="--compiler-options=-fvisibility=hidden,-fdata-sections,-ffunction-sections"
+    export CUDAFLAGS="--compiler-options=${CXXFLAGS// /,}"
 fi
 
 # Install system dependencies
-yum install -y \
-    openblas-devel \
-    openblas-static \
-    swig3
+yum install -y openblas-devel openblas-static swig3
 
 # Build and patch faiss
 cd faiss && \
@@ -53,10 +49,9 @@ cd faiss && \
         -DFAISS_ENABLE_GPU=${FAISS_ENABLE_GPU} \
         -DFAISS_ENABLE_PYTHON=OFF \
         -DBUILD_TESTING=OFF \
-        -DCMAKE_CUDA_ARCHITECTURES=${CMAKE_CUDA_ARCHITECTURES} \
         -DFAISS_OPT_LEVEL=${FAISS_OPT_LEVEL} \
         -DCMAKE_BUILD_TYPE=Release && \
-    cmake --build build --config Release -j3 && \
+    cmake --build build -j3 && \
     cmake --install build && \
     git apply ../patch/faiss-rename-swigfaiss.patch && \
     cd ..
