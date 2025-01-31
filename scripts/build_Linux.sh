746f9cc4cf21ec6660551c26fad29315c7fbf231
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2025-01-31 15:55:21 +0900

    Update faiss to v1.10.0 (#120)

--- scripts/build_Linux.sh
+++ scripts/build_Linux.sh
@@ -4,42 +4,6 @@ set -eux
 
 export CXXFLAGS="-fvisibility=hidden -fdata-sections -ffunction-sections"
 
-FAISS_ENABLE_GPU=${FAISS_ENABLE_GPU:-"OFF"}
-
-CUDA_VERSION=${CUDA_VERSION:-"12.3"}
-CUDA_PKG_VERSION=${CUDA_PKG_VERSION:-${CUDA_VERSION//./-}}
-NVIDIA_REPO_URL="http://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo"
-
-# Fix manylinux2014 aarch64
-if [[ $(uname -m) == "aarch64" ]]; then
-    yum -y install epel-release && yum repolist
-fi
-
-# Setup CUDA build environment
-if [[ ${FAISS_ENABLE_GPU} == "ON" ]]; then
-    echo "Installing CUDA toolkit"
-    yum -y install yum-utils && \
-        yum-config-manager --add-repo ${NVIDIA_REPO_URL} && \
-        yum repolist && \
-        yum -y install \
-            cuda-compiler-${CUDA_PKG_VERSION} \
-            cuda-libraries-devel-${CUDA_PKG_VERSION} \
-            cuda-nvprof-${CUDA_PKG_VERSION} \
-            devtoolset-7-gcc \
-            devtoolset-7-gcc-c++ \
-            devtoolset-7-gcc-gfortran \
-            devtoolset-7-binutils
-
-    ln -s cuda-${CUDA_VERSION} /usr/local/cuda && \
-        echo "/usr/local/cuda/lib64" >> /etc/ld.so.conf.d/cuda.conf && \
-        echo "/usr/local/nvidia/lib" >> /etc/ld.so.conf.d/nvidia.conf && \
-        echo "/usr/local/nvidia/lib64" >> /etc/ld.so.conf.d/nvidia.conf && \
-        ldconfig
-
-    export PATH="/opt/rh/devtoolset-7/root/usr/bin:/usr/local/cuda/bin:${PATH}"
-    export CUDAFLAGS="--compiler-options=${CXXFLAGS// /,}"
-fi
-
 # Check if swig is available
 if ! command -v swig &> /dev/null; then
     echo "swig is not available. Please install swig."
@@ -48,7 +12,7 @@ fi
 swig -version
 
 # Install system dependencies
-yum install -y openblas-devel openblas-static
+dnf install -y openblas-devel openblas-static
 
 # Build and patch faiss
 cd faiss && \
