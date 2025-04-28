fa8dbcbb8d05323fdf75231005147c28a957bd6a
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2025-04-28 11:17:38 +0900

    Remove cross-compiling setup

--- scripts/build_macOS.sh
+++ scripts/build_macOS.sh
@@ -2,13 +2,6 @@
 
 set -eux
 
-HOST_ARCH=${HOST_ARCH:-$(uname -m)}
-TARGET_ARCH=${TARGET_ARCH:-$HOST_ARCH}
-if [[ ${TARGET_ARCH} == auto* || ${TARGET_ARCH} == native ]]; then
-    TARGET_ARCH=${HOST_ARCH}
-fi
-echo "TARGET_ARCH=${TARGET_ARCH}"
-
 # Fix directory structure
 sudo mkdir -p /usr/local/include && \
     sudo chown -R $(whoami) /usr/local/include
@@ -18,32 +11,10 @@ sudo mkdir -p /usr/local/share && \
     sudo chown -R $(whoami) /usr/local/share
 
 # Install system dependencies
-brew install swig
+brew install swig libomp
 
-# Build libomp: needed for cross compilation
-function build_libomp() {
-    echo "Building libomp"
-    git clone \
-            --depth 1 \
-            --filter=blob:none \
-            --sparse \
-            --branch ${LLVM_VERSION:-"llvmorg-19.1.1"} \
-            https://github.com/llvm/llvm-project.git \
-            third-party/llvm-project && \
-        cd third-party/llvm-project && \
-        git sparse-checkout set openmp cmake && \
-        cd openmp && \
-        cmake . \
-            -B build \
-            -DCMAKE_BUILD_TYPE=Release \
-            -DCMAKE_OSX_ARCHITECTURES=${TARGET_ARCH} \
-            -DLIBOMP_ENABLE_SHARED=ON && \
-        cmake --build build -j && \
-        cmake --install build && \
-        cd ../../..
-}
-
-build_libomp
+# Workaround for libomp
+export OpenMP_ROOT=$(brew --prefix)/opt/libomp
 
 # Build and patch faiss
 echo "Building faiss"
@@ -54,8 +25,7 @@ cd faiss && \
         -DFAISS_ENABLE_PYTHON=OFF \
         -DBUILD_TESTING=OFF \
         -DFAISS_OPT_LEVEL=${FAISS_OPT_LEVEL:-"generic"} \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_OSX_ARCHITECTURES=${TARGET_ARCH} && \
+        -DCMAKE_BUILD_TYPE=Release && \
     cmake --build build --config Release -j && \
     cmake --install build && \
     git apply ../patch/faiss-rename-swigfaiss.patch && \
