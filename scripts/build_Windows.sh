face927f93876481d0f7839ee4dd31e726cff4ec
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2025-07-28 11:22:10 +0900

    Add a patch to fix MSVC error C3016

--- scripts/build_Windows.sh
+++ scripts/build_Windows.sh
@@ -23,26 +23,19 @@ if [[ "$PROCESSOR_IDENTIFIER" == ARM* ]]; then
     # NOTE: PROCESSOR_ARCHITECTURE is incorrectly set to "AMD64" on emulated ARM64 Windows runners.
     install_openblas woa64-dll
     CMAKE_GENERATOR_PLATFORM="ARM64"
-    CMAKE_GENERATOR_TOOLSET="v143"  # Use MSVC toolset for ARM64
-    CMAKE_CXX_FLAGS=""
 else
-    echo "Installing OpenBLAS for x86_64..."
-    # conda install -y -c conda-forge --override-channels openblas
     install_openblas x64
     CMAKE_GENERATOR_PLATFORM="x64"
-    CMAKE_GENERATOR_TOOLSET="v143"  # Use MSVC toolset for x86_64
-    CMAKE_CXX_FLAGS=""
-    # CMAKE_GENERATOR_TOOLSET="ClangCL"
-    # CMAKE_CXX_FLAGS="-D_CRT_SECURE_NO_WARNINGS -Wno-unused-function -Wno-format"
 fi
 
 # Build and patch faiss
 cd third-party/faiss && \
     git apply ../../patch/faiss-remove-lapack.patch && \
+    git apply ../../patch/faiss-fix-omp-loop-signed-index.patch && \
     cmake . \
         -B build \
         -A $CMAKE_GENERATOR_PLATFORM \
-        -T $CMAKE_GENERATOR_TOOLSET \
+        -T v143 \
         -DFAISS_ENABLE_GPU=OFF \
         -DFAISS_ENABLE_PYTHON=OFF \
         -DFAISS_OPT_LEVEL=${FAISS_OPT_LEVEL:-"generic"} \
@@ -50,7 +43,7 @@ cd third-party/faiss && \
         -DCMAKE_PREFIX_PATH="${CMAKE_PREFIX_PATH}" \
         -DCMAKE_BUILD_TYPE=Release \
         -DBLA_STATIC=ON \
-        -DCMAKE_CXX_FLAGS=$CMAKE_CXX_FLAGS && \
+        -DCMAKE_CXX_FLAGS="/EHsc" && \
     cmake --build build --config Release -j && \
     cmake --install build --prefix "${CMAKE_PREFIX_PATH}" && \
     git apply ../../patch/faiss-rename-swigfaiss.patch && \
